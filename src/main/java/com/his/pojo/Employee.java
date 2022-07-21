b9d67477562267b780157ea5dfed97eba3fe41ad
MaRuncheng <MaRuncheng0@gmail.com>
2022-07-21 15:11:35 +0800

    init

---
+++ src/main/java/com/his/pojo/Employee.java
@@ -0,0 +1,89 @@
+package com.his.pojo;
+
+
+public class Employee {
+
+  private long id;
+  private long deptmentId;
+  private long registLevelId;
+  private long schedulingId;
+  private String realname;
+  private String password;
+  private long delmark;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getDeptmentId() {
+    return deptmentId;
+  }
+
+  public void setDeptmentId(long deptmentId) {
+    this.deptmentId = deptmentId;
+  }
+
+
+  public long getRegistLevelId() {
+    return registLevelId;
+  }
+
+  public void setRegistLevelId(long registLevelId) {
+    this.registLevelId = registLevelId;
+  }
+
+
+  public long getSchedulingId() {
+    return schedulingId;
+  }
+
+  public void setSchedulingId(long schedulingId) {
+    this.schedulingId = schedulingId;
+  }
+
+
+  public String getRealname() {
+    return realname;
+  }
+
+  public void setRealname(String realname) {
+    this.realname = realname;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public long getDelmark() {
+    return delmark;
+  }
+
+  public void setDelmark(long delmark) {
+    this.delmark = delmark;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+            "id=" + id +
+            ", deptmentId=" + deptmentId +
+            ", registLevelId=" + registLevelId +
+            ", schedulingId=" + schedulingId +
+            ", realname='" + realname + '\'' +
+            ", password='" + password + '\'' +
+            ", delmark=" + delmark +
+            '}';
+  }
+}
